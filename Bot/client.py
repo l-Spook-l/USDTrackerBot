fa68bc180bf51c7261221f6945701b6272db515d
l-Spook-l <uaspookua@gmail.com>
2024-05-06 21:50:23 +0300

    A button for obtaining exchange rates has been added.

--- Bot/client.py
+++ Bot/client.py
@@ -1,9 +1,17 @@
-from aiogram.types import Message
-from config import dp
-from aiogram.filters import CommandStart
+import os
+from aiogram import Router, F
+from aiogram.types import Message, FSInputFile
+from aiogram.filters import CommandStart, Command
+from DataBase.database_operations import get_data_database
+
+from .client_buttons import start_kb
+
+client_router = Router()
 
 
-@dp.message(CommandStart())
+@client_router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("Привіт")
+    await message.answer("Привіт", reply_markup=start_kb)
+
+
 
