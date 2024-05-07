a3f798979cc60f713030c769bd5b4469c9447074
l-Spook-l <uaspookua@gmail.com>
2024-05-07 14:18:25 +0300

    Implemented retrieval of data from the database for the current date.

--- DataBase/database_operations.py
+++ DataBase/database_operations.py
@@ -1,4 +1,5 @@
-from sqlalchemy import insert, select
+from datetime import date
+from sqlalchemy import insert, select, func
 from .models import CurrencyRate
 from .config_db import session
 
@@ -15,4 +16,15 @@ def add_data_database(data: dict) -> bool:
         return False
 
 
-
+def get_data_database():
+    # SELECT * from currency_rate_table where DATE(Timestamp) = CURRENT_DATE;
+    try:
+        with session() as s:
+            # Получаем текущую дату
+            query = select(CurrencyRate).filter(func.Date(CurrencyRate.Timestamp) == date.today())
+            res = s.execute(query)
+            result = res.all()
+        return result
+    except Exception as error:
+        print(f"Error occurred while reading data: {error}")
+        return False
