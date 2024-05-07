e519eaa04e0ca74cd76a3180eda326a4513e2dc2
l-Spook-l <uaspookua@gmail.com>
2024-05-07 14:14:34 +0300

    Implemented more informative message for the 'start' command.

--- Bot/client.py
+++ Bot/client.py
@@ -3,7 +3,7 @@ from aiogram import Router, F
 from aiogram.types import Message, FSInputFile
 from aiogram.filters import CommandStart, Command
 from DataBase.database_operations import get_data_database
-
+from utils import create_excel_from_db
 from .client_buttons import start_kb
 
 client_router = Router()
@@ -11,7 +11,9 @@ client_router = Router()
 
 @client_router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("Привіт", reply_markup=start_kb)
+    await message.answer("Вітаю! Для отримання інформації про динаміку курсу долара до гривні, "
+                         "ви можете ввести команду - /get_exchange_rate, або натиснути кнопку - Get exchange rate",
+                         reply_markup=start_kb)
 
 
 
