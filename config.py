c81e2c6e2564af64b3457941ddc880cb4d5d3bd5
l-Spook-l <uaspookua@gmail.com>
2024-05-07 14:33:15 +0300

    The sequence of imports in the file has been changed

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-from aiogram import Bot, Dispatcher
 import os
+from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 load_dotenv()
+from Bot.client import client_router
 
-
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=os.getenv('TOKEN'))  # опрос серверов telegram на наличие обновлений
 dp = Dispatcher()  # обработка входящих обновлений
+dp.include_router(client_router)
