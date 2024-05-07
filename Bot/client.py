12d5b41e479d3c029d42da51a4c4844ac2596252
l-Spook-l <uaspookua@gmail.com>
2024-05-07 14:16:48 +0300

    Implemented sending an Excel file to the user upon the command /get_exchange_rate and clicking the 'Get exchange rate' button.

--- Bot/client.py
+++ Bot/client.py
@@ -16,4 +16,18 @@ async def cmd_start(message: Message):
                          reply_markup=start_kb)
 
 
+@client_router.message(Command('get_exchange_rate'))
+@client_router.message(F.text.lower() == 'get exchange rate')
+async def get_exchange_rate(message: Message):
+    data = get_data_database()
+    create_excel_from_db(data)
 
+    # Получаем текущий рабочий каталог
+    current_directory = os.getcwd()
+
+    # Путь к файлу для отправки
+    file_path = os.path.join(current_directory, "Today's currency exchange rate.xlsx")
+
+    await message.reply_document(
+        document=FSInputFile(path=file_path)
+    )
