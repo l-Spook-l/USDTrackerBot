9b7d38ccafdf0a54c648910296a8f7bd4e0eecfc
l-Spook-l <uaspookua@gmail.com>
2024-05-07 14:25:54 +0300

    Added condition for launching the parser itself.

--- Parser/google_finance_parser.py
+++ Parser/google_finance_parser.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 def get_data() -> dict:
     # URL страницы Google Finance
     url = "https://www.google.com/finance/quote/USD-UAH"
+    # url = "https://www.google.com/finance/quote/EUR-UAH"
 
     # Загрузка страницы
     response = requests.get(url)
@@ -13,11 +14,10 @@ def get_data() -> dict:
     # currency_div = soup.find("div", {"data-source": "USD", "data-target": "UAH", "data-last-price": "39.25688"})
     currency_div = soup.find("div", {"data-source": True, "data-target": True, "data-last-price": True})
 
-    print(currency_div["data-last-price"], type(currency_div["data-last-price"]))
     rate = round(float(currency_div["data-last-price"]), 4)
-    print(rate)
-    print(currency_div["data-source"])
-    print(currency_div["data-target"])
+    # print(rate)
+    # print(currency_div["data-source"])
+    # print(currency_div["data-target"])
 
     return {
         "Currency_pair": f"{currency_div['data-source']}-{currency_div['data-target']}",
@@ -26,4 +26,4 @@ def get_data() -> dict:
 
 
 if __name__ == "__main__":
-    print(get_data())
+    get_data()
